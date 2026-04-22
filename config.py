fc34bd9c1b56ee017a40f100e39ae92252645aa0
Seb <155646893+CyberSecSleuth@users.noreply.github.com>
2026-04-22 09:35:20 +0530

    TOKEN AND ID

---
+++ config.py
@@ -0,0 +1,37 @@
+# =============================================================================
+# BOT
+# =============================================================================
+
+BOT_TOKEN = "YOUR_TOKEN_HERE"
+
+# =============================================================================
+# GUILD & CHANNELS
+# =============================================================================
+
+GUILD_ID = YOUR_GUILD_ID_HERE  # Main Discord Server ID
+
+WELCOME_CHANNEL_IDS = [CHANNEL_ID_1, CHANNEL_ID_2]  # Welcome channels
+RULES_CHANNEL_ID = YOUR_CHANNEL_ID  # Rules channel
+ROLES_CHANNEL_ID = YOUR_CHANNEL_ID  # Roles channel
+GAME_CHANNEL_ID = YOUR_CHANNEL_ID  # Word guess game channel
+CONFESSION_CHANNEL_ID = YOUR_CHANNEL_ID  # confessions channel
+SOCIAL_CHANNEL_ID = YOUR_CHANNEL_ID  # Instagram media channel
+
+NO_BOT_USE_CHANNEL_ID = [YOUR_CHANNEL_ID_1, YOUR_CHANNEL_ID_2]  # Channel to not respond/auto-react to
+
+# =============================================================================
+# ROLES & USERS
+# =============================================================================
+
+BOT_ACCESS_ROLE = YOUR_ROLE_ID  # Role required to use bot commands
+
+NO_BOT_USE_USER_ID = [YOUR_USER_ID_1, YOUR_USER_ID_2]  # Bot user to not respond to
+
+# =============================================================================
+# EMOJIS
+# =============================================================================
+
+HICHAT = "<:EMOJI:EMOJI_ID>"  # Hi greeting emoji
+BOW = "<:EMOJI:EMOJI_ID>"  # Welcome bow emoji
+MINECAT = "<:EMOJI:EMOJI_ID>"  # Minecraft cat emoji
+ALRIGHT = "<:EMOJI:EMOJI_ID>"  # Alright/scoom emoji
